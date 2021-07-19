4ec5454511a14306eaea4cd26c55e4f317d837f3
minhdc <minhdc@fabbi.io>
2021-07-19 23:10:10 +0700

    refactor master

---
+++ README.md
@@ -0,0 +1,1 @@
+# Consumers for face recognition service
--- utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def face_distance(face_encodings, face_to_compare):
-	if len(face_encodings) == 0:
-		return np.empty((0))
-	face_dist_value = np.linalg.norm(face_encodings - face_to_compare, axis=1)
-	print('[Face Services | face_distance] Distance between two faces is {}'.format(
-		face_dist_value))
-	return face_dist_value
