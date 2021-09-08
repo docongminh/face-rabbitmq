855a27cd269d59ea152c72d3aa77efa207a176e9
minhdc <minhdc@fabbi.io>
2021-09-08 14:32:49 +0700

    refactor

--- common/base64.h
+++ common/base64.h
@@ -8,8 +8,8 @@ namespace base64 {
      */
     
 
-    std::string base64_encode     (std::string const& s, bool url = false);
-    std::string base64_encode_pem (std::string const& s);
+    std::string base64_encode(std::string const& s, bool url = false);
+    std::string base64_encode_pem(std::string const& s);
     std::string base64_encode_mime(std::string const& s);
 
     std::string base64_decode(std::string const& s, bool remove_linebreaks = false);
